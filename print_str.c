d5f8d6320d5fcbe652309f2bf147f213720c986b
hamza <you@example.com>
2023-04-24 10:57:56 -0700

    creating a function to print a sring

---
+++ print_str.c
@@ -0,0 +1,15 @@
+#include"main.h"
+#include <stdarg.h>
+/**
+ * print_string - Prints a character
+ * @args: List of arguments
+ *
+ * Return: Number of characters printed
+ */
+int print_string(va_list args)
+{
+	char *str = va_arg(args, char *);
+
+	_puts(str);
+	return (_strlen(str));
+}
