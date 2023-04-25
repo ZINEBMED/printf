9d3649b53bee2fcdc861ddeb9308a7327cb05af3
hamza ouhamouch <ouhamouchhamza@gmail.com>
2023-04-25 14:24:07 -0700

    update comments

--- _printf.c
+++ _printf.c
@@ -44,7 +44,7 @@ int _printf(const char *format, ...)
 /**
  * get_func - the function that call the functions
  * @c: the format to check
- * Return: its depends on the format n
+ * Return: its depends on the format
  */
 
 int (*get_func(char c))(va_list)
